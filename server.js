98739771bac2fd4cb080439cf151997b1e0048ce
KimGaHyeon <kimgahyeon1@ajou.ac.kr>
2019-09-24 14:53:18 +0900

    정상작동 확인 commit

--- server.js
+++ server.js
@@ -27,15 +27,11 @@ app.get('/', (req, res)=>{
     res.sendFile(__dirname + '/index.html');
 })
 
-// create mate
-app.post('/mate', async(req, res)=>{
-    const email = req.body.email;
-    console.log("email: " + email);
+async function cc_call(fn_name, args){
+    
     const walletPath = path.join(process.cwd(), 'wallet');
     const wallet = new FileSystemWallet(walletPath);
-    console.log(`Wallet path: ${walletPath}`);
 
-    // Check to see if we've already enrolled the user.
     const userExists = await wallet.exists('user1');
     if (!userExists) {
         console.log('An identity for the user "user1" does not exist in the wallet');
@@ -46,7 +42,49 @@ app.post('/mate', async(req, res)=>{
     await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: false } });
     const network = await gateway.getNetwork('mychannel');
     const contract = network.getContract('teamate');
-    const result = await contract.submitTransaction('addUser', email);
+
+    var result;
+    
+    if(fn_name == 'addUser')
+        result = await contract.submitTransaction('addUser', args);
+    else if( fn_name == 'addRating')
+    {
+        e=args[0];
+        p=args[1];
+        s=args[2];
+        result = await contract.submitTransaction('addRating', e, p, s);
+    }
+    else if(fn_name == 'readRating')
+        result = await contract.evaluateTransaction('readRating', args);
+    else
+        result = 'not supported function'
+
+    return result;
+}
+
+// create mate
+app.post('/mate', async(req, res)=>{
+    const email = req.body.email;
+    console.log("add mate email: " + email);
+
+    result = cc_call('addUser', email)
+
+    const myobj = {result: "success"}
+    res.status(200).json(myobj) 
+})
+
+// add score
+app.post('/score', async(req, res)=>{
+    const email = req.body.email;
+    const prj = req.body.project;
+    const sc = req.body.score;
+    console.log("add project email: " + email);
+    console.log("add project name: " + prj);
+    console.log("add project score: " + sc);
+
+    var args=[email, prj, sc];
+    result = cc_call('addRating', args)
+
     const myobj = {result: "success"}
     res.status(200).json(myobj) 
 })
@@ -73,6 +111,8 @@ app.post('/mate/:email', async (req,res)=>{
     const result = await contract.evaluateTransaction('readRating', email);
     const myobj = JSON.parse(result)
     res.status(200).json(myobj)
+    // res.status(200).json(result)
+
 });
 
 // server start
